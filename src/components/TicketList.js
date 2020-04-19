8572ff40c9ba9b25cc73b828d06e7830857c87f3
mtaylorpdx <taylor.matt@protonmail.com>
2020-04-19 11:05:54 -0700

    follow along

---
+++ src/components/TicketList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+const masterTicketList = [
+  {
+    names: 'Moost and Squirrel',
+    location: '3A',
+    issue: 'Boris is at it again'
+  },
+  {
+    names: 'Jekyl and Hyde',
+    location: '4B',
+    issue: 'Night terrors'
+  },
+  {
+    names: 'Tom and Jerry',
+    location: '1D',
+    issue: 'The usual'
+  }
+];
+
+
+
+function Ticketlist(){
+  return (
+    <React.Fragment>
+    <hr/>
+    {masterTicketList.map((ticket, index) =>
+      <Ticket names={ticket.names}
+        location={ticket.location}
+        issue={ticket.issue}
+        key={index}/>
+      )}
+      </React.Fragment>
+      );
+}
+
+export default Ticketlist;
